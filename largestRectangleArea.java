5bffec411a1a5a10a885e82036793782b00b3537
cqn2219076254 <71445886+cqn2219076254@users.noreply.github.com>
2021-03-10 13:01:48 +0800

    Update largestRectangleArea.java
    单调栈

--- largestRectangleArea.java
+++ largestRectangleArea.java
@@ -1,3 +1,32 @@
-public class largestRectangleArea {
+import java.util.*;
 
+public class  largestRectangleArea {
+    public static void main(String[] args) {
+        System.out.println(largestRectangleArea(new int[]{2, 1, 5, 6, 3, 4}));
+    }
+
+    public static int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(right, n);
+
+        Deque<Integer> mono_stack = new LinkedList<>();
+        for (int i = 0; i < n; ++i) {
+            while (!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]) {
+                right[mono_stack.peek()] = i;
+                mono_stack.pop();
+            }
+            left[i] = (mono_stack.isEmpty() ? -1 : mono_stack.peek());
+            mono_stack.push(i);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int area = (right[i] - left[i] - 1) * heights[i];
+            if(area > ans){
+                ans = area;
+            }
+        }
+        return ans;
+    }
 }
